1300d93357239ce212065edd38c5435c6ef78346
Raghava2016 <raghava.juvvaji@gmail.com>
2019-06-10 17:48:58 -0700

    graphql using node js and express server

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/gql-ninja');
+
+mongoose.connection.once('open',() => {
+  console.log('connected to database');
+});
+
+app.use('/graphql',graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000,() => {
+  console.log('now listneing on port 4000')
+})
